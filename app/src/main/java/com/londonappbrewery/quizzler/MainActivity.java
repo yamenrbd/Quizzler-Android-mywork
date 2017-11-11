0b2aaf63849aaf17e3b7f97fec3edef0733ab7a2
yamen_rbd <yamenrbd@gmail.com>
2017-11-11 11:13:28 +0300

    from work

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -19,26 +19,28 @@ public class MainActivity extends Activity {
     TextView question ;
     ProgressBar mProgressBar ;
     TextView score ;
+    int mQuestion;
+    int mIndex;
 
 
 
 
     // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +51,13 @@ public class MainActivity extends Activity {
         question = (TextView)findViewById(R.id.question_text_view);
         mProgressBar = (ProgressBar)findViewById(R.id.progress_bar);
         score = (TextView)findViewById(R.id.score);
+
+
+        mQuestion = mQuestionBank[mIndex].getQuestionId();
+        question.setText(mQuestion);
+
+
+
 
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
